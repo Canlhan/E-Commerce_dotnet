c97c889a286b220f0f8742091a948f7ed14dc3a6
Burak <burakguven351999@gmail.com>
2022-06-26 18:19:57 +0300

    xD

---
+++ E-Commerce/Models/Kategori.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Models
+{
+    public class Kategori
+    {
+        public int KategoriId { get; set; }
+        public string KategoriAd { get; set; }
+    }
+}
---
+++ E-Commerce/Models/Ürün.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Models
+{
+    public class Ürün
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+
+    }
+}
